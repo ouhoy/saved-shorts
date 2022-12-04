33dacb56401b11d0ecc09d31f91563e917773446
ouhoy <kimkinon@gmail.com>
2022-12-04 23:30:06 +0100

    Add Starting files

---
+++ Content.js
@@ -0,0 +1,3 @@
+console.log("Hello from your extention");
+document.body.insertAdjacentHTML("beforeEnd", `<h1>Hello World</h1>`);
+document.body.insertAdjacentHTML("beforeBegin", `<h1>Hello World</h1>`);
