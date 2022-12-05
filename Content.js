414a1394ddcd7aff7b65fe42268cabd4176fae00
ouhoy <kimkinon@gmail.com>
2022-12-05 21:15:22 +0100

    Work on YT DOM loading problem
    --The body loads without the needed buttons div which makes it problematic to insert the save short button

--- Content.js
+++ Content.js
@@ -1,3 +1,30 @@
-console.log("Hello from your extention");
-document.body.insertAdjacentHTML("beforeEnd", `<h1>Hello World</h1>`);
-document.body.insertAdjacentHTML("beforeBegin", `<h1>Hello World</h1>`);
+function $(id, slectAll = false) {
+  return slectAll ? document.querySelectorAll(id) : document.querySelector(id);
+}
+
+console.log("Yooo!");
+
+let myBtns = $("#like-button");
+
+// window.addEventListener("load", () => {
+//   const likeBtns = $("#like-button");
+//   const htmlMarkup = `<button> Hello</button>`;
+//   console.log(myBtns);
+//   //   likeBtns.insertAdjacentHTML("beforeEnd", htmlMarkup);
+//   console.log("added");
+// });
+
+if (document.readyState !== "loading") {
+  console.log("the btns ", $("#like-button"));
+  console.log("gota wait");
+  setTimeout(() => {
+    console.log("the btns ", $("#like-button"));
+  }, 5000);
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("document was not ready, place code here");
+    console.log(myBtns);
+  });
+}
+
+function myInitCode() {}
