13a695c66f3f933508f56248b15f18aef70c5778
ouhoy <kimkinon@gmail.com>
2023-08-18 23:23:20 +0100

    Add sample options page

---
+++ options/options.ts
@@ -0,0 +1,11 @@
+chrome.storage.local.get(["savedShorts"]).then((result) => {
+
+    const {body} = document;
+
+
+    result.savedShorts.forEach((short: ShortDetails) => {
+        const shortHTML = `<iframe width="370" height="658" src="https://www.youtube.com/embed/${short.id}" title="${short.title}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+
+        body.insertAdjacentHTML("beforeend", shortHTML)
+    })
+});
