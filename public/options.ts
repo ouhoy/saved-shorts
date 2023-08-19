6e7a839ba0c761f29b57b3f7b856db348647581d
ouhoy <kimkinon@gmail.com>
2023-08-19 23:55:33 +0100

    fix shots container element issue

--- public/options.ts
+++ public/options.ts
@@ -7,21 +7,16 @@ const resetButton = $(".reset") as HTMLElement;
 
 chrome.storage.local.get(["savedShorts"]).then((result) => {
 
-    const shortsContainer = $(".shorts") as HTMLElement;
-    result.savedShorts.forEach((short: ShortDetails) => {
-        const shortHTML = `<iframe width="370" height="658" src="https://www.youtube.com/embed/${short.id}" title="${short.title}" 
-                                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
-                                    picture-in-picture; web-share" allowfullscreen></iframe>`;
 
-        shortsContainer.insertAdjacentHTML("beforeend", shortHTML)
+    result.savedShorts.forEach((short: ShortDetails) => {
+
+        const shortHTML = `<iframe width="370" height="658" src="https://www.youtube.com/embed/${short.id}" title="${short.title}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
+
+        ($(".shorts") as HTMLElement).insertAdjacentHTML("beforeend", shortHTML)
     })
 });
 
 resetButton.addEventListener("click", () => {
-    console.log("Clicked!")
-    chrome.storage.local.set({savedShorts: []}).then(() => {
 
-        console.log("OK!")
-
-    });
+    chrome.storage.local.set({savedShorts: []}).then(() => {});
 })
